b3f4222d41bc2ede3c9f7ae30f10f44717da7de1
simayaydin <simayaydin_@hotmail.com>
2026-05-18 15:18:06 +0300

    -

--- server/main.py
+++ server/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
